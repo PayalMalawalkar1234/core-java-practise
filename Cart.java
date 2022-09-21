abd882912651dc5ae7f939733cb4225c1b8e83c4
PayalMalawalkar1234 <111423241+PayalMalawalkar1234@users.noreply.github.com>
2022-09-21 17:12:44 +0530

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,14 +1,15 @@
 class Cart
+	
 {
 private String itemName;
 private double itemPrice;
 private String itemBrand;
 
-void cart()
+void cart()  //default constructor
 {
 
 }
-Cart(String itemName,double itemPrice,String itemBrand)
+Cart(String itemName,double itemPrice,String itemBrand)      //parameterised constructor
 {
 	System.out.println("-------constructor------");
 this.itemName=itemName;
@@ -17,19 +18,19 @@ this.itemPrice=itemPrice;
 
 }
 
-void display()
+void display()           // method to dispay Cart details
 {
 	System.out.println("Name of Item:"+itemName);
 	System.out.println("Name of The Brand:"+itemBrand);
 	System.out.println("Price of Item:"+itemPrice);
 	
 }
- 
+ // main method
  public static void main(String...args)
  {
 	 
 	 Cart c1= new Cart("Mobile",20000,"Xiomi");
-	 c1.display();
+	 c1.display();                                          // calling method to display
 	 
 	 
  }
